b54027419a47f09149787b4a105099d24620ad6e
Corpelijn <bassiecorpelijn@hotmail.com>
2016-10-14 12:04:10 +0200

    last changes before testing thursday

--- Server/Design4NatureServer/src/design4natureserver/Design4NatureServer.java
+++ Server/Design4NatureServer/src/design4natureserver/Design4NatureServer.java
@@ -36,7 +36,7 @@ import shared.Message;
  * @author Bas
  */
 public class Design4NatureServer extends Application implements Listener {
-
+    
     private GraphicsContext canvas;
     private int xp1 = -1;
     private int xp2 = -1;
@@ -44,14 +44,14 @@ public class Design4NatureServer extends Application implements Listener {
     private int yp2 = -1;
     private Map map;
     private Group window2nd;
-
+    
     @Override
     public void start(Stage primaryStage) {
         Thread t = new Thread(() -> {
             new Server().start();
         });
         t.start();
-
+        
         Thread serverInput = new Thread(() -> {
             while (true) {
                 Message msg = Server.getNextReceivedMessage();
@@ -63,12 +63,12 @@ public class Design4NatureServer extends Application implements Listener {
                 String[] message = msg.getData().split(",");
                 float lat = Float.parseFloat(message[0]);
                 float lon = Float.parseFloat(message[1]);
-
+                
                 String[] xy = d2xy(lat, lon).split(",");
                 if (xy[0].equals("buiten bereik")) {
                     continue;
                 }
-
+                
                 int x = Integer.parseInt(xy[0]);
                 int y = Integer.parseInt(xy[1]);
 
@@ -81,13 +81,33 @@ public class Design4NatureServer extends Application implements Listener {
                 }
 
                 //drawLineTo(msg.getSender(), x, y);
-                Platform.runLater(() -> {
-                    map.draw();
-                });
+//                Platform.runLater(() -> {
+//                    map.draw();
+//                });
+//                
+//                try {
+//                    Thread.sleep(10);
+//                } catch (InterruptedException ex) {
+//                    Logger.getLogger(Design4NatureServer.class.getName()).log(Level.SEVERE, null, ex);
+//                }
             }
         });
         serverInput.start();
-
+        
+        new Thread(() -> {
+            while (true) {
+                Platform.runLater(() -> {
+                    map.draw();
+                });
+                
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Design4NatureServer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }).start();
+        
         Group root = new Group();
         window2nd = new Group();
         Canvas canv = new Canvas(1920, 1080);
@@ -97,19 +117,19 @@ public class Design4NatureServer extends Application implements Listener {
         map.draw();
         //drawShapes();
         root.getChildren().add(canv);
-
+        
         Canvas adminDraw = new Canvas(970, 540);
         adminDraw.setLayoutX(100);
         adminDraw.setLayoutY(20);
         window2nd.getChildren().add(adminDraw);
-
+        
         new Thread(() -> {
             while (true) {
                 Platform.runLater(() -> {
                     WritableImage image = canv.snapshot(null, null);
                     adminDraw.getGraphicsContext2D().drawImage(image, 0, 0, 970, 540);
                 });
-
+                
                 try {
                     Thread.sleep(800);
                 } catch (InterruptedException ex) {
@@ -117,32 +137,32 @@ public class Design4NatureServer extends Application implements Listener {
                 }
             }
         }).start();
-
+        
         Stage secondStage = new Stage();
-
+        
         int index = 0;
         for (Screen screen : Screen.getScreens()) {
             Rectangle2D bounds = screen.getVisualBounds();
-
-            if (index == 0) {
+            
+            if (index == 1) {
                 primaryStage.setX(bounds.getMinX());
                 primaryStage.setY(bounds.getMinY());
-            } else if (index == 1) {
-                secondStage.setX(bounds.getMinX());
+            } else if (index == 0) {
+                secondStage.setX(bounds.getMinX() + 800);
                 secondStage.setY(bounds.getMinY());
             }
             index++;
         }
-
-        primaryStage.setScene(new Scene(root, Color.BLACK));
+        
+        primaryStage.setScene(new Scene(root, Color.LIGHTGRAY));
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
-
+        
         secondStage.setScene(new Scene(window2nd));
         //secondStage.setMaximized(true);
         secondStage.show();
     }
-
+    
     private String d2xy(float lat, float lon) {
         float tmplat = lat;
         float tmplng = lon;
@@ -150,15 +170,15 @@ public class Design4NatureServer extends Application implements Listener {
         double Y = Math.round(gps2Y(tmplat, tmplng));
         return 50.57 < tmplat && 53.63 > tmplat && 3.29 < tmplng && 7.58 > tmplng ? ((int) X + "," + (int) Y) : "buiten bereik";
     }
-
+    
     private double gps2X(double b, double c) {
         double a = 0;
-
+        
         double lat0 = 52.1551744;
         double lng0 = 5.38720621;
         double X0 = 155000;
         double Y0 = 463000;
-
+        
         double dlat = 0.36 * (b - lat0);
         double dlng = 0.36 * (c - lng0);
         for (int i = 1; 10 > i; i++) {
@@ -166,15 +186,15 @@ public class Design4NatureServer extends Application implements Listener {
         }
         return X0 + a;
     }
-
+    
     private double gps2Y(double b, double c) {
         double a = 0;
-
+        
         double lat0 = 52.1551744;
         double lng0 = 5.38720621;
         double X0 = 155000;
         double Y0 = 463000;
-
+        
         double dlat = 0.36 * (b - lat0);
         double dlng = 0.36 * (c - lng0);
         for (int i = 1; 11 > i; i++) {
@@ -182,7 +202,7 @@ public class Design4NatureServer extends Application implements Listener {
         }
         return Y0 + a;
     }
-
+    
     private void drawLineTo(int sender, int x, int y) {
         Platform.runLater(() -> {
             if ((xp1 != -1 && sender == 1) || (xp2 != -1 && sender == 2)) {
@@ -192,13 +212,13 @@ public class Design4NatureServer extends Application implements Listener {
                     canvas.setStroke(Color.GREEN);
                 }
                 canvas.setLineWidth(1);
-
+                
                 canvas.strokeLine(
                         sender == 1 ? xp1 : xp2,
                         sender == 1 ? yp1 : yp2,
                         x, y);
             }
-
+            
             if (sender == 1) {
                 xp1 = x;
                 yp1 = y;
@@ -215,13 +235,13 @@ public class Design4NatureServer extends Application implements Listener {
     public static void main(String[] args) {
         launch(args);
     }
-
+    
     @Override
     public void onCollision(PlayerCollision collisionInfo) {
         System.out.println(collisionInfo);
         Server.killClient(collisionInfo.player.getId());
     }
-
+    
     @Override
     public void onAddPlayer(Player player) {
         Platform.runLater(() -> {
@@ -231,14 +251,14 @@ public class Design4NatureServer extends Application implements Listener {
             btn.setBackground(new Background(new BackgroundFill(player.getColor(), CornerRadii.EMPTY, Insets.EMPTY)));
             btn.setLayoutY(5 + player.getId() * 30);
             btn.setOnAction(new EventHandler<ActionEvent>() {
-
+                
                 @Override
                 public void handle(ActionEvent event) {
                     Server.killClient(player.getId());
                     btn.setDisable(true);
                 }
             });
-
+            
             window2nd.getChildren().add(btn);
         });
     }
